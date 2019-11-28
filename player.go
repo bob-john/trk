b14cdaf6032cb5b71f9c1ba52b568f6626d29692
bob-john <bob.john@hotmail.fr>
2019-11-28 22:27:45 +0100

    wip: player

--- player.go
+++ player.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"trk/track"
 
 	"github.com/gomidi/midi"
@@ -10,67 +11,87 @@ import (
 
 type Player struct {
 	ports map[string]mid.Out
+	last  *playEventSet
+	next  *playEventSet
 }
 
 func NewPlayer() *Player {
-	return &Player{make(map[string]mid.Out)}
+	return &Player{make(map[string]mid.Out), new(playEventSet), new(playEventSet)}
 }
 
 func (p *Player) Play(trk *track.Track, tick int) {
 	parts, _ := track.Parts(trk)
 	for _, part := range parts {
-		p.playPattern(trk, part, tick)
-		p.playMute(trk, part, tick)
+		p.writePattern(trk, part, tick)
+		p.writeMute(trk, part, tick)
 	}
+	p.flush()
 }
 
 func (p *Player) PlayPattern(trk *track.Track, tick int) {
 	parts, _ := track.Parts(trk)
 	for _, part := range parts {
-		p.playPattern(trk, part, tick)
+		p.writePattern(trk, part, tick)
 	}
+	p.flush()
 }
 
 func (p *Player) PlayMute(trk *track.Track, tick int) {
 	parts, _ := track.Parts(trk)
 	for _, part := range parts {
-		p.playMute(trk, part, tick)
+		p.writeMute(trk, part, tick)
 	}
+	p.flush()
 }
 
-func (p *Player) playPattern(trk *track.Track, part *track.Part, tick int) {
+func (p *Player) writePattern(trk *track.Track, part *track.Part, tick int) {
 	pattern := track.Pattern(trk, part, tick)
-	p.write(part.ProgChgPortOut, channel.Channel(part.ProgChgOutCh).ProgramChange(uint8(pattern)))
+	p.write(part.ProgChgPortOut, p.pattern(part, pattern))
 }
 
-func (p *Player) playMute(trk *track.Track, part *track.Part, tick int) {
+func (p *Player) pattern(part *track.Part, pattern int) midi.Message {
+	return channel.Channel(part.ProgChgOutCh).ProgramChange(uint8(pattern))
+}
+
+func (p *Player) writeMute(trk *track.Track, part *track.Part, tick int) {
 	mute := track.Mute(trk, part, tick)
 	for n, ch := range part.TrackCh {
 		if ch == -1 {
 			continue
 		}
-		if mute[n] {
-			p.write(part.MutePortOut, channel.Channel(ch).ControlChange(94, 1))
-		} else {
-			p.write(part.MutePortOut, channel.Channel(ch).ControlChange(94, 0))
-		}
+		p.write(part.MutePortOut, p.mute(part, ch, mute[n]))
 	}
 }
 
+func (p *Player) mute(part *track.Part, ch int, muted bool) midi.Message {
+	if muted {
+		return channel.Channel(ch).ControlChange(94, 1)
+	}
+	return channel.Channel(ch).ControlChange(94, 0)
+}
+
 func (p *Player) write(ports []string, message midi.Message) {
-	required := make(map[string]bool)
-	for _, name := range ports {
-		required[name] = true
-		out, ok := p.ports[name]
+	p.next.Insert(ports, message)
+}
+
+func (p *Player) flush() {
+	curr := p.next.Substract(p.last)
+	for _, e := range curr.Events {
+		port, ok := p.ports[e.Port]
 		if !ok {
 			var err error
-			out, err = mid.OpenOut(driver, -1, name)
+			port, err = mid.OpenOut(driver, -1, e.Port)
 			if err != nil {
 				continue
 			}
-			p.ports[name] = out
+			p.ports[e.Port] = port
 		}
-		out.Send(message.Raw())
+		port.Send(e.Message)
+	}
+
+	required := make(map[string]bool)
+	for _, e := range p.next.Events {
+		required[e.Port] = true
 	}
 	for name, port := range p.ports {
 		if !required[name] {
@@ -79,4 +100,49 @@ func (p *Player) write(ports []string, message midi.Message) {
 			}
 		}
 	}
+
+	p.last, p.next = p.next, new(playEventSet)
+}
+
+type playEvent struct {
+	Port    string
+	Message []byte
+}
+
+func (e playEvent) Equals(o playEvent) bool {
+	return e.Port == o.Port && bytes.Equal(e.Message, o.Message)
+}
+
+type playEventSet struct {
+	Events []playEvent
+}
+
+func (s *playEventSet) Insert(ports []string, message midi.Message) {
+	for _, port := range ports {
+		e := playEvent{port, message.Raw()}
+		if s.contains(e) {
+			return
+		}
+		s.Events = append(s.Events, e)
+	}
+}
+
+func (s *playEventSet) Substract(o *playEventSet) *playEventSet {
+	r := new(playEventSet)
+	for _, e := range s.Events {
+		if o.contains(e) {
+			continue
+		}
+		r.Events = append(r.Events, e)
+	}
+	return r
+}
+
+func (s *playEventSet) contains(e playEvent) bool {
+	for _, o := range s.Events {
+		if o.Equals(e) {
+			return true
+		}
+	}
+	return false
 }
