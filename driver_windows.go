623b9857370959d8d133c54eee3c51e715fc933e
bob-john <bob.john@hotmail.fr>
2019-11-29 21:44:30 +0100

    rtmidi test

--- driver_windows.go
+++ driver_windows.go
@@ -6,13 +6,40 @@ import "C"
 
 import (
 	"fmt"
+	"os"
 	"syscall"
 	"unsafe"
+	"time"
 
 	"gitlab.com/gomidi/midi/mid"
+	"trk/rtmidi"
 )
 
 func NewDriver() (mid.Driver, error) {
+	in, err := rtmidi.NewMIDIInDefault()
+	must(err)
+	// defer in.Destroy()
+	count,err := in.PortCount()
+	must(err)
+	for i := 0; i < count; i++ {
+		name, err := in.PortName(i)
+		must(err)
+		fmt.Printf("- [%d] %s\n", i,name)
+	}
+	must(in.OpenPort(0, ""))
+	// defer in.Close()
+	must(in.IgnoreTypes(false, false, false))
+	go func() {
+		for  {
+			m, t, err := in.Message()
+			must(err)
+			if len(m) != 0 {
+			fmt.Println(m,t)
+		}
+		}
+	}()
+	time.Sleep(3*time.Second)
+	os.Exit(0)
 	return &winMidiDriver{}, nil
 }
 
